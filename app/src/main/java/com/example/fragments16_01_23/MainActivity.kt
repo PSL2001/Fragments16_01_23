f865fd626363162fa64b0d944db4d91da9642cb3
Pablo Sánchez López <65461723+PSL2001@users.noreply.github.com>
2023-01-16 13:18:42 +0100

    Añadido fragment con codigo

--- app/src/main/java/com/example/fragments16_01_23/MainActivity.kt
+++ app/src/main/java/com/example/fragments16_01_23/MainActivity.kt
@@ -2,10 +2,24 @@ package com.example.fragments16_01_23
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.add
+import androidx.fragment.app.commit
+import com.example.fragments16_01_23.fragments.BlueFragment
+import com.example.fragments16_01_23.fragments.RedFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        cargarFragments()
+    }
+
+    private fun cargarFragments() {
+        supportFragmentManager.commit {
+            setReorderingAllowed(true)
+            add<BlueFragment>(R.id.fcvBlu)
+            add<RedFragment>(R.id.fcvRed)
+            addToBackStack("add")
+        }
     }
 }
