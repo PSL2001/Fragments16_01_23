271960acae479c2a48ed8d963a47e97e9fdf00a8
Pablo Sánchez López <65461723+PSL2001@users.noreply.github.com>
2023-01-16 13:37:32 +0100

    Añadido funcionalidad en el boton del fragmento rojo

--- app/src/main/java/com/example/fragments16_01_23/fragments/RedFragment.kt
+++ app/src/main/java/com/example/fragments16_01_23/fragments/RedFragment.kt
@@ -1,32 +1,22 @@
 package com.example.fragments16_01_23.fragments
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import com.example.fragments16_01_23.R
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [RedFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class RedFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+
+    var listener: InterfazBotonesFragment? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+
         }
     }
 
@@ -38,23 +28,21 @@ class RedFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_red, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment RedFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            RedFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val boton = view.findViewById<Button>(R.id.btn_red)
+        boton.setOnClickListener {
+            listener?.botonClick("Fragment Red")
+        }
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        if (context is InterfazBotonesFragment) listener = context
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+        listener = null
     }
 }
